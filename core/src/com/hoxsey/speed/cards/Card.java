02eb3f34ae3d16db3ba0d8dae2136df054551903
Joseph Hoxsey <JosephHoxsey@gmail.com>
2017-02-09 03:17:39 -0800

    - Added Card and Deck class - Added content to Card class

---
+++ core/src/com/hoxsey/speed/cards/Card.java
@@ -0,0 +1,56 @@
+package com.hoxsey.speed.cards;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Hoxsey on 2/9/2017.
+ */
+public class Card {
+
+    public final static int SPADES = 0;
+    public final static int HEARTS = 1;
+    public final static int DIAMONDS = 2;
+    public final static int CLUBS = 3;
+
+    public final static int ACE = 1;
+    public final static int JACK = 11;
+    public final static int QUEEN = 12;
+    public final static int KING = 13;
+
+    private Vector2 position;
+    private Texture image;
+    private int suit;
+    private int value;
+
+    public Card(int suit, int value, int x, int y)   {
+        position = new Vector2(x,y);
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public int getSuit()    {
+        return suit;
+    }
+
+    public int getValue()    {
+        return value;
+    }
+
+    public String getSuitString()  {
+        switch(suit) {
+            case SPADES:
+                return "Spades";
+            case HEARTS:
+                return "Hearts";
+            case DIAMONDS:
+                return "Diamonds";
+            case CLUBS:
+                return "Clubs";
+        }
+    }
+
+    public String toString()   {
+        return value+" of "+getSuitString();
+    }
+}
---
+++ core/src/com/hoxsey/speed/cards/Deck.java
@@ -0,0 +1,7 @@
+package com.hoxsey.speed.cards;
+
+/**
+ * Created by Hoxsey on 2/9/2017.
+ */
+public class Deck {
+}
