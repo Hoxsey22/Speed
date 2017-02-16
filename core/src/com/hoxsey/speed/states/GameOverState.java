57247e96984ddd2905cc025c0facae1f80a5daa1
Joseph Hoxsey <JosephHoxsey@gmail.com>
2017-02-16 12:48:26 -0800

    - Added gameover(bugs) - flip deck can be used

---
+++ core/src/com/hoxsey/speed/states/GameOverState.java
@@ -0,0 +1,50 @@
+package com.hoxsey.speed.states;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.hoxsey.speed.Speed;
+import com.hoxsey.speed.states.State;
+
+/**
+ * Created by Hoxsey on 2/16/2017.
+ */
+public class GameOverState extends State {
+
+    public Texture background;
+    public Texture statusImage;
+
+
+    public GameOverState(StateManager sm, int status)  {
+        super(sm);
+        background = new Texture("playing_table.png");
+        if(status < 1)    {
+            statusImage = new Texture("winner.png");
+        }
+        else
+            statusImage = new Texture("lose.png");
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+            sb.draw(background, 0,0, Speed.WIDTH, Speed.HEIGHT);
+            sb.draw(statusImage, Speed.WIDTH/2 - statusImage.getWidth(), Speed.HEIGHT/2 - statusImage.getHeight());
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
