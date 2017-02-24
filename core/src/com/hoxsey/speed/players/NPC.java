acbc30d6ab51046df2e0b223598e5f05b3d5b483
Joseph Hoxsey <JosephHoxsey@gmail.com>
2017-02-24 08:04:55 -0800

    - blind

--- core/src/com/hoxsey/speed/players/NPC.java
+++ core/src/com/hoxsey/speed/players/NPC.java
@@ -1,8 +1,11 @@
 package com.hoxsey.speed.players;
 
+import com.badlogic.gdx.utils.Timer;
 import com.hoxsey.speed.cards.Card;
 import com.hoxsey.speed.cards.Deck;
 import com.hoxsey.speed.cards.Hand;
+
+import java.awt.event.ActionListener;
 
 /**
  * Created by Hoxsey on 2/19/2017.
@@ -14,12 +17,20 @@ public class NPC {
     private int difficulty;
     private boolean flipFlag;
     private Card selectedCard;
+    private Timer timer;
 
-    public NPC()    {
+    public NPC()   {
         deck = new Deck();
         hand = new Hand(2);
         difficulty = 1;
         flipFlag = false;
+        timer = new Timer();
+        timer.scheduleTask(new Timer.Task() {
+            @Override
+            public void run() {
+
+            }
+        },1,1);
     }
 
     public void setupHand() {
@@ -44,6 +55,24 @@ public class NPC {
 
     public boolean isFlipFlagged()  {
         return flipFlag;
+    }
+
+    public void nextCard()  {
+        int position = hand.getHand().indexOf(selectedCard);
+
+        if(deck.isEmpty() && hand.size() == 1) {
+            return;
+        }
+        else if(hand.size() == 1) {
+            hand.draw();
+            selectedCard = hand.getCardAt(position+1);
+        }
+        else if(position == hand.size()-1)  {
+            selectedCard = hand.getCardAt(0);
+        }
+        else {
+            selectedCard = hand.getCardAt(position+1);
+        }
     }
 
     public void playCardHelper()  {
@@ -82,6 +111,13 @@ public class NPC {
             return true;
         return false;
     }
+
+    private Timer.Task task = new Timer.Task() {
+        @Override
+        public void run() {
+            nextCard();
+        }
+    };
 
 
 }
